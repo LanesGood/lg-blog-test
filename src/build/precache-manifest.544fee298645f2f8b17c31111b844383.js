9541a1d75cfe97a6302b5dac3f6cd96846e7034b
Lane Goodman <Lane@developmentseed.org>
2020-09-09 11:45:14 -0400

    first commit

---
+++ src/build/precache-manifest.544fee298645f2f8b17c31111b844383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a3e0d9a1ac7e151b5256e3a57b846f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788b1888aa260ba6bde5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a4e88d001a55fd7a651b",
+    "url": "/static/js/2.65a20c30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65a20c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788b1888aa260ba6bde5",
+    "url": "/static/js/main.2fdb16f9.chunk.js"
+  },
+  {
+    "revision": "42cd862d0b356f44cadb",
+    "url": "/static/js/runtime-main.930842e3.js"
+  }
+]);
